8b847bf5ef6072d7149841426b63068510cfc187
Gaurav Pandey <76164354+THEGAURAVPANDEY@users.noreply.github.com>
2023-03-22 11:51:43 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>XenonBlade</h1>
+        <p>@all right reserved </p>
+      </div>
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/gauravpandey11/"
+            target={"blank"}
+          >
+            LinkedIn
+          </a>
+          <a href="https://vercel.com/dashboard" target={"blank"}>
+            Vercel
+          </a>
+          <a href="https://github.com/THEGAURAVPANDEY" target={"blank"}>
+            Github
+          </a>{" "}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
